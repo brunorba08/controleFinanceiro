6a9d0aa3fb4a6d5d2c6565d040428b8e6e290231
Bruno <bruno.ribeiro@itpower.co>
2025-08-12 14:35:05 -0300

    Adicionar arquivos de projeto.

---
+++ controleFinanceiro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace controleFinanceiro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
